7b7d1b8ed0657028109d69c9cb7eb747795bcb99
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2024-03-21 12:53:04 +0330

    shahnaz

--- 1-edition/2.py
+++ 1-edition/2.py
@@ -70,3 +70,120 @@ def stop_clients():
         client.stop()
 
 # -----------------ClientManager PART End-------------------#
+
+# -----------------Helper PART Start-------------------#
+from time import sleep
+RETRY_COUNT = 2
+        
+def get_group_id( client, group_username):
+    retry = 0
+    try:
+        group_info =  client.get_chat(group_username)
+        return group_info.id
+    except Exception as e:
+        print(f"Exception: {e}")
+        sleep_bitch(1)
+        if retry <= RETRY_COUNT:
+            retry += 1
+            get_group_id()
+        else:
+                print("fix the fucking bug first. then come back")
+                exit()
+
+
+def get_origin_and_dest_chat_id():
+    retry = 0
+    try:
+        # start client just for get chats ids
+        client = start_helper_client()
+        # get data from user and pass the chats ids
+        origin_group_username = input("origin_group_username please bitch: ")
+        destination_group_username = input("destination_group_username please bitch: ")
+        origin_group_id = get_group_id(client, origin_group_username)
+        destination_group_id = get_group_id(client, destination_group_username)
+        # stop the client
+        stop_helper_client(client)
+        return origin_group_id,destination_group_id
+    except Exception as e:
+            print(f"Exception: {e}")
+            sleep_bitch(1)
+            if retry <= RETRY_COUNT:
+                retry += 1
+                get_origin_and_dest_chat_id()
+            else:
+                print("fix the fucking bug first. then come back")
+                exit()
+    
+def start_helper_client():
+    retry = 0
+    try:
+        api_id = 27356729
+        api_hash = "2076532de16fc82d242fcc1a012ce5f1"
+        client = Client("666mineTGTGBMAmine", api_id=api_id, api_hash=api_hash)
+        client.start()
+        return client
+    except Exception as e:
+        print(f"Exception: {e}")
+        sleep_bitch(1)
+        if retry <= RETRY_COUNT:
+            retry += 1
+            start_helper_client()
+        else:
+            print("fix the fucking bug first. then come back")
+            exit()
+        
+    
+def stop_helper_client(client):
+    retry = 0
+    try:
+        client.stop()
+        client.disconnect()
+    except Exception as e:
+        print(f"Exception: {e}")
+        sleep_bitch(1)
+        if retry <= RETRY_COUNT:
+            retry += 1
+        stop_helper_client()
+    else:
+        print("fix the fucking bug first. then come back")
+        exit()
+
+def sleep_bitch(second):
+        sleep(second)
+# -----------------Helper PART End-------------------#
+        
+# -----------------Scraper PART Start-------------------#
+from pyrogram import errors
+
+def scrape_and_add_members(origin_group_id,destination_group_id):
+    # Start all clients
+    start_clients()
+
+    # Scrape members from origin group
+    for client in CLIENTS:
+        for member in client.get_chat_members(origin_group_id):
+            # Check if user is already scraped
+            if not is_user_scraped(member.user.id):
+                try:
+                    # Add user to contacts
+                    client.add_contact(member.user.id, member.user.username)
+                    # Add user to destination group
+                    client.add_chat_members(destination_group_id, member.user.id)
+                except errors.PeerFloodError:
+                    print(f"""PEER_FLOOD error encountered. Probably this client is limited , check it,\n
+                        client data: {client.get_me()}\n
+                        Waiting for 5 seconds before changing client.""")
+                    sleep(5) # Wait for 60 seconds before retrying
+                    break                        
+                finally:
+                    # Save user to database
+                    save_user(member.user.id, member.user.username)
+    # Stop all clients
+    stop_clients()
+
+# -----------------Scraper PART End-------------------#
+    
+
+if __name__ == "__main__":
+    
+    pass
